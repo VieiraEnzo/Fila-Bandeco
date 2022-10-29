ac5d9a3975722be3423241c59d854f45942fafb1
Enzo <vieira.enzof@gmail.com>
2022-10-29 19:55:26 -0300

    <control>>

--- control.py
+++ control.py
@@ -1,7 +1,16 @@
 class Control:
 
+    #Checa se o login do aluno é valido
     def valida_senha_aluno(self, identificador,senha):
-        pass
+        if: ##########econtrou no banco de dados
+            return render_template("aluno_main.html")
+        else:
+            return render_template("index.html", popup = "block", erro = "erroLOGIN")
 
+    #Registra o aluno dentro do Banco de Dados
     def registrar_aluno(self, nome, cpf, telefone, email, dre, senha):
-        pass
+        if: ###### cpf ja dentro do banco de dados
+            return render_template("regis_aluno.html", erro = "erroJALOGADO")
+        else:
+            ##################### adicionar no banco de dados
+            return render_template("index.html", popup = "block")
