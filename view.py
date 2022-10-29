7183b0d29d5949db1538265834e0e0812630c525
Anonytario <agl-clps@hotmail.com>
2022-10-29 18:03:10 -0300

    <regis_aluno>

--- view.py
+++ view.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 #Pagina inicial do projeto
 @app.route("/")
 def index():
-    return render_template("index.html", var = "of")
+    return render_template("index.html", popup = "none")
 
 
 @app.route("/login")
 def index_login():
-    return render_template("index.html", var = "on")
+    return render_template("regis_aluno.html", popup = "block")
     
 
 app.run()
