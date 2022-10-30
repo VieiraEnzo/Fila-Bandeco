65bb8e0d0cbc127b5ddd79cab1802b1cd03f6caf
Anonytario <agl-clps@hotmail.com>
2022-10-30 16:45:21 -0300

    <cu>

--- db_select.py
+++ db_select.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('filaRU.db')
 c = conn.cursor()
 
-j = c.execute("select * from aluno").fetchall()
+j = c.execute("select rowid, * from aluno").fetchall()
 conn.commit()
 
 print(j)
